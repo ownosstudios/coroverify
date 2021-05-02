0e30b9091c0c913f776de2907963661231a055b4
Yuvraj Sharma <yuvrajtest@outlook.com>
2021-05-02 21:08:10 +0530

    API docs for authentication created

--- controller/auth/index.js
+++ controller/auth/index.js
@@ -4,26 +4,26 @@ const UserToken = require('models/newUserToken')
 
 const authPost = (req, res) => {
     const uid = req.body.token
-    const claims = {...req.body.claims}
+    const claims = {userType: req.body.claims}
     const cookieOptions = {
         maxAge: Date.now(),
         httpOnly: true,
         secure: true,
     }
-    firebaseAdmin.auth().createCustomToken(uid, claims)
+    firebaseAdmin.auth().setCustomUserClaims(uid, claims)
     .then(async (customToken) => {
-        const newUser = new UserToken({token: customToken, _id: req.body.email})
+        // const newUser = new UserToken({token: customToken, _id: req.body.email})
         try {
             await newUser.save()
             res.cookie('token', customToken, cookieOptions)
-            res.status(200).json({token: customToken})
+            res.status(200).json({token: customToken, message: 'Registration successful', status: 200})
         } catch (e) {
-            console.log(e)
+            // console.log(e)
             res.status(500).json({...e})
         }
     })
     .catch((err) => {
-        console.log(err)
+        // console.log(err)
         res.status(500).json({...err})
     })
 }
@@ -52,15 +52,19 @@ const updateAuth = (req, res) => {
 }
 
 const getAuth = (req, res) => {
-    console.log(req)
+    // console.log(req)
     res.status(200).json({message: 'good shizz'})
 }
 
 const login = async (req, res) => {
+    if (req.cookies.token) {
+        res.status(400).json({message: 'Already logged in', status: 400})
+    }
     try {
         const user = await UserToken.findOne({_id: req.body.email})
         if (user) {
-            res.status(200).json({message: 'Found user', ...user})
+            res.cookie('token', user.token)
+            res.status(200).json({message: 'Found user', status: 200, token: user.token})
         }
     } catch (e) {
         res.status(404).json({message: 'No user found', status: 404})
@@ -76,7 +80,7 @@ const logout = async (req, res) => {
             throw Error()
         }
     } catch (e) {
-        res.status(500).json({message: 'No user found.'})
+        res.status(404).json({message: 'No user found.'})
     }
 }
 
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const {authPost, authMiddleware, login, getAuth, logout} = require('controller/auth')
 
-router.get('/', getAuth)
+// router.get('/', getAuth)
 router.post('/register', authPost)
 router.post('/login', login)
 router.get('/logout', logout)
